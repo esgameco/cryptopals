d92e9d18136c9968758fb02f4d3a6a60bdfea598
Chance Vodnoy <esgameco@gmail.com>
2021-08-08 23:28:50 -0700

    Added bytes class and cmake

--- 01set/lib/bytes.h
+++ 01set/lib/bytes.h
@@ -21,23 +21,38 @@ enum class BytesFormat
     Bytes Class:
 
     Stores bytes as a vector of 8-bit unsigned integers in the form of ascii characters.
+    Can get representation as hex or base64.
 */
 class Bytes
 {
 public:
-    // Constructor methods
+    // Constructor Methods
     template<typename T>
     Bytes(const T&, const BytesFormat&);
     template<typename T>
     void updateBytes(const T&, const BytesFormat&);
 
+    // Display in Encoded Formats
+    std::string getAsciiString();
+    std::string getHexString();
+    std::string getBase64String();
+
+    // Decode Ascii to Formats
+    void decodeFrom(const BytesFormat&);
+
     // Format Conversions
-    void convertTo(const BytesFormat&);
+    void convertFrom(const BytesFormat&);
 
 private:
     // Private Conversion Methods
     void convertToAsciiFromHex();
+    void convertToAsciiFromBase64();
 
+    // Private Decoding Methods
+    void decodeHexFromAscii();
+    void decodeBase64FromAscii();
+
+    // Member Variables
     BytesFormat format;
     std::vector<uint8_t> bytes;
 };
